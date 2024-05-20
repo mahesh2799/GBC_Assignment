2b544b430ed92904a7ad1fdcfaaa8bd9a33b3f00
mahesh2799 <mdadheech27@gmail.com>
2024-05-19 22:41:01 -0400

    Corrected code

--- ex1_test.py
+++ ex1_test.py
@@ -1,4 +1,4 @@
-import write_your_name as name_point
+import Mahesh_Dadheech as name_point
 
 def test_hi_my_name_is():
     assert len(name_point.hi_my_name_is())  > 1 
--- write_your_name (2) (1).py
+++
@@ -1,2 +0,0 @@
-def hi_my_name_is():
-    return "" # Write your name inside the quotation marks
